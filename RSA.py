f0a6b2a5af8c5271bece2a7cdd0d606f87c8f3d7
= <=>
2022-12-01 15:40:55 -0500

    Comments explaining prime generation algorithms

--- RSA.py
+++ RSA.py
@@ -20,39 +20,43 @@ def nBitRandom(n):
 def getLowLevelPrime(n):
     # Generate a prime candidate not divisible by first primes
     while True:
-        # Obtain a random number
+        # get random number of n bits
         prime_candidate = nBitRandom(n)
  
-         # Test divisibility by pre-generated
-         # primes
         for divisor in first_primes_list:
             if prime_candidate % divisor == 0 and divisor ** 2 <= prime_candidate:
                 break
         else: return prime_candidate
  
 # Probabalistic primality test
-def isMillerRabinPassed(mrc):
-    # Run 20 iterations of Rabin Miller Primality test
-    maxDivisionsByTwo = 0
-    ec = mrc-1
-    while ec % 2 == 0:
-        ec >>= 1
-        maxDivisionsByTwo += 1
-    assert(2**maxDivisionsByTwo * ec == mrc-1)
+# The more rounds, the more probable a number is prime
+def isMillerRabinPassed(candidate, iterations):
+    # Want to find numbers s and d so that candidate - 1 = (2^s)*d
+    s = 0
+    d = candidate - 1
+    while d % 2 == 0:
+        d >>= 1
+        s += 1
+    assert(2**s * d == candidate-1)
  
-    def trialComposite(round_tester):
-        if pow(round_tester, ec, mrc) == 1:
+    # Seeing if we can find a witness that proves our candidate is composite
+    # If a number fails to pass fermat's little theorem (a^(n-1) congruent to 1 mod n)
+    # and fails to pass (a^((2^r)*d) congruent to -1 mod n) for some 0 <= r <= s
+    # Then we know that the number is composite
+    # Otherwise, it is probably prime! 
+    def trialComposite(a):
+        if pow(a, d, candidate) == 1:
             return False
-        for i in range(maxDivisionsByTwo):
-            if pow(round_tester, 2**i * ec, mrc) == mrc-1:
+        for i in range(s):
+            if pow(a, 2**i * d, candidate) == candidate-1:
                 return False
         return True
  
-    # Set number of trials here
-    numberOfRabinTrials = 20
-    for i in range(numberOfRabinTrials):
-        round_tester = random.randrange(2, mrc)
-        if trialComposite(round_tester):
+    # For the total number of iterations, run our helper method
+    for i in range(iterations):
+        # get a random number between 2 and our candidate to 
+        a = random.randrange(2, candidate)
+        if trialComposite(a):
             return False
     return True
  
@@ -62,8 +66,8 @@ def generate_prime():
         n = 10
         # Low level primality test
         prime_candidate = getLowLevelPrime(n)
-        # High level primality test
-        if not isMillerRabinPassed(prime_candidate):
+        # High level primality test, 5 iterations
+        if not isMillerRabinPassed(prime_candidate, 5):
             continue
         else:
             return prime_candidate
@@ -75,34 +79,33 @@ def gcd(a, b):
     return a
 
 # Euclid's extended algorithm for finding the multiplicative inverse of two numbers (modular)
-def modInverse(A, M):
-    m0 = M
-    y = 0
+# Requires the two numbers to be coprime
+# we're solving (ax + by) = gcd(a, b), which should be 1 if they're coprime
+# You can rewrite this as ax - 1 = (-y)m
+# This is the same as saying ax is congruent to 1 mod m, so x is a modular multiplicative inverse of a 
+def modInverse(a, b):
+    m = b
     x = 1
- 
-    if (M == 1):
+    y = 0
+    if (b == 1):
         return 0
- 
-    while (A > 1):
- 
-        # q is quotient
-        q = A // M
- 
-        t = M
- 
-        # m is remainder now, process
-        # same as Euclid's algo
-        M = A % M
-        A = t
+    while (a > 1):
+        # find whole number of times b goes into a
+        q = a // b
+        # set temp variable
+        t = b
+        # b becomes the remainder
+        b = a % b
+        # a becomes the original b
+        a = t
+        # set temp variable to y
         t = y
- 
         # Update x and y
         y = x - q * y
         x = t
- 
     # Make x positive
     if (x < 0):
-        x = x + m0
+        x = x + m
  
     return x
 
@@ -110,37 +113,37 @@ def modInverse(A, M):
 def generate_keypair(p, q):
     n = p * q
 
-    #Phi is the totient of n
+    # Phi is the totient of n
     phi = (p-1) * (q-1)
 
-    #Choose an integer e such that e and phi(n) are coprime
+    # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
 
-    #Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
 
-    #Use Extended Euclid's Algorithm to generate the private key
+    # Use Extended Euclid's Algorithm to generate the private key
     d = modInverse(e, phi)
     
-    #Return public and private keypair
-    #Public key is (e, n) and private key is (d, n)
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
 
 def encrypt(pk, plaintext):
-    #Unpack the key into it's components
+    # Unpack the key into it's components
     key, n = pk
-    #Convert each letter in the plaintext to numbers based on the character using a^b mod m
+    # Convert each letter in the plaintext to numbers based on the character using a^b mod m
     cipher = [(ord(char) ** key) % n for char in plaintext]
-    #Return the array of bytes
+    # Return the array of bytes
     return cipher
 
 def decrypt(pk, ciphertext):
-    #Unpack the key into its components
+    # Unpack the key into its components
     key, n = pk
-    #Generate the plaintext based on the ciphertext and key using a^b mod m
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
     plain = [chr((char ** key) % n) for char in ciphertext]
-    #Return the array of bytes as a string
+    # Return the array of bytes as a string
     return ''.join(plain)
